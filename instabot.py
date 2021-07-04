0aad93e0562f63d2fd3fb64dad310b7f323b4482
Josef Mueller <j.mueller5021@gmail.com>
2021-07-04 18:19:23 +0200

    instabot.py: can now read and print messages.

--- instabot.py
+++ instabot.py
@@ -1,11 +1,21 @@
+"""
+Inspired by https://github.com/CamTosh/instagram-bot-dm
+I did not use his code on purpose, but if I did all credit goes to him.
+"""
+
 import os
 import time
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from random import randint
+import re
+from typing import List, Any, Iterator
+
 import dotenv
 import pyotp
+import selenium.common.exceptions
 
 from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class InstaBotServer(BaseHTTPRequestHandler):
@@ -25,9 +35,14 @@ class InstaBot:
         self.username = username
         self.password = password
         self.two_fa = two_fa
-        self.driver = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        mobile_emulation = {
+            "userAgent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
+        }
+        options.add_experimental_option("mobileEmulation", mobile_emulation)
+        self.driver = webdriver.Chrome(options=options)
         self.login()
-        Helper.random_sleep(15)
+        Helper.random_sleep()
 
     def login(self):
         # goto instagram
@@ -41,6 +56,7 @@ class InstaBot:
         # insert username and password
         self.driver.find_element_by_xpath('//input[@name="username"]').send_keys(self.username)
         self.driver.find_element_by_xpath('//input[@name="password"]').send_keys(self.password)
+        Helper.random_sleep()
         self.driver.find_element_by_xpath('//button[@type="submit"]').click()
 
         Helper.random_sleep()
@@ -60,25 +76,39 @@ class InstaBot:
         Helper.random_sleep()
 
         # deactivate notifications
-        notification_button = self.driver.find_element_by_xpath('/html/body/div[3]/div/div/div/div[3]/button[2]')
-        if notification_button is not None:
+        try:
+            notification_button = self.driver.find_element_by_xpath('/html/body/div[3]/div/div/div/div[3]/button[2]')
             notification_button.click()
+        except selenium.common.exceptions.NoSuchElementException:
+            pass
 
+        # find all messages
+        visited = []
+        while True:
+            links_to_persons = self.driver.find_elements_by_xpath('//a[count(div[@aria-labelledby])>0][@href]')
+            # person_ids = [re.findall(r'\d+', person.get_attribute('href'))[0] for person in links_to_persons]
+            for link in links_to_persons:
+                person_id = re.findall(r'\d+', link.get_attribute('href'))[0]
+                if person_id not in visited:
+                    visited.append(person_id)
+                    link.click()
+                    Helper.random_sleep(2)
+                    messages = self.driver.find_elements_by_xpath('//div[@role="listbox"]//*/span')
+                    for message in messages:
+                        print(message.text)
+                    self.driver.get('https://www.instagram.com/direct/inbox/')
+                    Helper.random_sleep(2)
+                    break
 
-        messages = self.driver.find_elements_by_xpath(
-            '//*[@id="react-root"]/section/div/div[2]/div/div/div[1]/div[3]/div/div/div/div')
-        print(messages)
 
     def automated_mode(self):
-        while True:
-            self.check_messages()
-            Helper.random_sleep()
+        self.check_messages()
 
 
 class Helper:
 
     @staticmethod
-    def random_sleep(min_sleep=5, max_sleep=7):
+    def random_sleep(min_sleep=3, max_sleep=5):
         time.sleep(randint(min_sleep, max_sleep + min_sleep))
 
 
