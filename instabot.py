7ac7eca285a3429fc1971030df85db4b6f6744d2
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-01-02 13:31:14 +0100

    add files from old device.

--- instabot.py
+++ instabot.py
@@ -3,25 +3,29 @@ Inspired by https://github.com/CamTosh/instagram-bot-dm
 I did not use his code on purpose, but if I did all credit goes to him.
 """
 
+import io
+import json
 import os
+import sys
 import re
 import time
 import random
 import uuid
 import textwrap
-# server
 from http.server import BaseHTTPRequestHandler
 from typing import List
-
 import dotenv
 import pyotp
+import string
+import codecs
+import mimetypes
 from PIL import Image, ImageDraw, ImageFont
-
-# bot
+from requests.api import head
 import selenium.common.exceptions
 from selenium import webdriver
+from instagram_private_api_extensions import media
 from selenium.webdriver.remote.webelement import WebElement
-import language_tool_python
+import requests as req
 
 
 class InstaBotServer(BaseHTTPRequestHandler):
@@ -43,14 +47,23 @@ class InstaBot:
         self.username = username
         self.password = password
         self.two_fa = two_fa
-        options = webdriver.ChromeOptions()
+
+        self.user_agent = 'Mozilla/5.0 (Linux; Android 10; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36'
+        self.rollout_hash = ''
+
+        # using mobile client
         mobile_emulation = {
-            "userAgent": 'Mozilla/5.0 (Linux; Android 10; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36'
+            "userAgent": self.user_agent
         }
+        options = webdriver.ChromeOptions()
         options.add_experimental_option("mobileEmulation", mobile_emulation)
         self.driver = webdriver.Chrome(options=options)
 
     def login(self):
+        """
+        login to instagram
+        """
+
         # goto instagram
         self.driver.get('https://www.instagram.com/accounts/login/')
 
@@ -61,10 +74,10 @@ class InstaBot:
         Helper.random_sleep()
 
         # insert username and password
-        self.driver.find_element_by_xpath(
-            '//input[@name="username"]').send_keys(self.username)
-        self.driver.find_element_by_xpath(
-            '//input[@name="password"]').send_keys(self.password)
+        self._send_keys("login: entering username ...",
+                        '//input[@name="username"]', self.username)
+        self._send_keys("login: entering password ...",
+                        '//input[@name="password"]', self.password)
         Helper.random_sleep()
         self._click_button("login: submitting form ...",
                            '//button[@type="submit"]')
@@ -72,17 +85,26 @@ class InstaBot:
         # generate and insert 2fa key
         if self.two_fa:
             Helper.random_sleep()
-            totp = pyotp.TOTP(os.getenv('INSTA_SECRET'))
-            self.driver.find_element_by_xpath(
-                '//input[@name="verificationCode"]').send_keys(totp.now())
+            totp = pyotp.TOTP(os.getenv('INSTA_SECRET')).now()
+            self._send_keys("login: entering 2fa key " + totp +
+                            " ...", '//input[@name="verificationCode"]', totp)
             self._click_button(
-                "login: submitting 2fa key ...", '//form/div[2]/button')
+                "login: submitting 2fa key " + totp + " ...", '//form/div[2]/button')
 
         Helper.random_sleep()
 
-    def check_messages(self):
-        # goto inbox
-        self.driver.get('https://www.instagram.com/direct/inbox/')
+        self.get_rollout_hash()
+
+    def check_messages(self, message_type='inbox'):
+        """
+        check messages and create post if certain keywords are found in the message
+        """
+        if message_type == 'inbox':
+            # goto inbox
+            self.driver.get('https://www.instagram.com/direct/inbox/')
+        elif message_type == 'request':
+            # goto requests
+            self.driver.get('https://www.instagram.com/direct/requests/')
 
         Helper.random_sleep()
 
@@ -115,24 +137,104 @@ class InstaBot:
                 # check if in message.text are certain keywords from list
                 if any(keyword in message.text for keyword in keywords):
                     # create image with text
-                    Helper.create_image(message.text)
+                    Helper.create_image(message.text, name=person_id)
                 print(message.text)
 
             self.driver.back()
 
     def _click_button(self, message, element):
+        # find and click button
         try:
             print(message)
-            button = self.driver.find_element_by_xpath(element)
-            button.click()
+            self.driver.find_element_by_xpath(element).click()
         except selenium.common.exceptions.NoSuchElementException:
             pass
 
-    def create_post(self):
-        pass
+    def _send_keys(self, message, element, text):
+        # find input and send keys
+        try:
+            print(message)
+            self.driver.find_element_by_xpath(element).send_keys(text)
+        except selenium.common.exceptions.NoSuchElementException:
+            pass
+
+    def get_rollout_hash(self):
+        """
+        get rollout hash from source code
+        """
+        self.driver.get('https://www.instagram.com/')
+        Helper.random_sleep()
+        page_source = self.driver.page_source
+        rollout_hash = re.findall(r'"rollout_hash":\s*"([^"]+)"', page_source)
+        if rollout_hash is not None and len(rollout_hash) > 0:
+            self.rollout_hash = rollout_hash[0]
+            print("get_rollout_hash: " + self.rollout_hash + " ...")
+            return self.rollout_hash
+        print("get_rollout_hash: no rollout_hash found ...")
+        return None
+
+    def create_post(self, img_id: str):
+        # create post on instagram
+
+        print("create_post: starting to add post ...")
+        # https://habr.com/ru/post/486714/
+
+        img_path = "images/" + img_id + ".jpg"
+
+        # read binary data from image
+        print("create_post: opened img " + img_id + " ...")
+        data = open(img_path, "rb").read()
+        width, height = Image.open(img_path).size
+
+        cookies = json.dumps(self.driver.get_cookies())
+
+        microseconds: str = str(int(time.time() * 1000))
+        instagram_rupload_params = json.dumps(
+            {"media_type": 1, "upload_id": microseconds, "upload_media_height": height, "upload_media_width": width})
+        headers = {
+            "User-agent": self.user_agent,
+            "Accept": "*/*",
+            'Accept-Encoding': 'gzip, deflate',
+            "Content-Type": "image/jpeg",
+            "Offset": "0",
+            "x-csrftoken": self.driver.get_cookie('csrftoken')['value'],
+            "x-entity-name": "fb_uploader_" + microseconds,
+            "x-entity-length": str(os.path.getsize(img_path)),
+            "x-instagram-ajax": self.rollout_hash,
+            "x-ig-app-id": "1217981644879628",
+            'x-instagram-rupload-params': instagram_rupload_params,
+            "cookie": cookies
+        }
+
+        response = req.post('https://www.instagram.com/rupload_igphoto/fb_uploader_' +
+                            microseconds, headers=headers, data=data)
+        print(response.headers)
+        print(response.text)
+
+        headers = {
+            "content-type": "application/x-www-form-urlencoded",
+            "user-agent": self.user_agent,
+            "x-csrftoken": "",
+            "x-ig-app-id": "1217981644879628",
+            "cookie": cookies
+        }
+
+        form_data = {
+            "source_type": "library",
+            "caption": "Test",
+            "upload_id": microseconds,
+            "usertags": "Test",
+            "custom_accessibility_caption": "Test",
+            "disable_comments": "1"
+        }
+
+        response = req.post(
+            'https://www.instagram.com/create/configure/', headers=headers, files=form_data)
+        print(response)
 
     def automated_mode(self):
-        self.check_messages()
+        self.check_messages('requests')
+        self.check_messages('inbox')
 
     def __del__(self):
         self.driver.close()
@@ -140,22 +242,21 @@ class InstaBot:
 
 class Helper:
 
-    @staticmethod
+    @ staticmethod
     def random_sleep(min_sleep=3, max_sleep=5):
         # random sleep to make sure that bot is not detected
         sleep_time = random.randint(min_sleep, max_sleep)
         print("random_sleep: sleeping for " + str(sleep_time) + " seconds ...")
         time.sleep(sleep_time)
 
-    @staticmethod
-    def create_image(message, post_type='normal'):
+    @ staticmethod
+    def create_image(message, img_id: str = None, post_type='normal'):
         # create image with text using PIL
         background_color = (45, 45, 45)
         normal_post = (251, 154, 111)
 
         # remove any special characters such as unicode characters from message
         message = Helper.clean_string(message)
-        # message = Helper.fix_spelling(message)
 
         # select color depending on post_type
         if post_type == 'normal':
@@ -163,14 +264,18 @@ class Helper:
         else:
             text_color = (255, 255, 255)
 
+        # create image
         W, H = (1080, 1080)
         img = Image.new(mode='RGB', size=(W, H), color=background_color)
         draw = ImageDraw.Draw(img)
 
-        font = ImageFont.truetype("/usr/local/share/fonts/a/Avenir_Book.ttf", size=40)
+        # set font
+        font = ImageFont.truetype(
+            "/usr/local/share/fonts/a/Avenir_Book.ttf", size=40)
 
         # wrap lines
         lines = textwrap.wrap(message, width=40)
+
         # calculate start of text in image
         # start from middle of image and go back half the line length
         y_text = W / 2 - font.getmetrics()[0] * (len(lines) / 2)
@@ -179,14 +284,16 @@ class Helper:
         # https://stackoverflow.com/a/56205095 thank you https://github.com/franck-dernoncourt
         for line in lines:
             line_width, line_height = font.getsize(line)
-            draw.text(((image_width - line_width) / 2, y_text), 
-                    line, font=font, fill=text_color)
+            draw.text(((image_width - line_width) / 2, y_text),
+                      line, font=font, fill=text_color)
             y_text += line_height
 
-        file_name = 'images/' + str(uuid.uuid4()) + '.png'
+        if img_id is None:
+            img_id = str(uuid.uuid4())
+        file_name = 'images/' + img_id + '.jpg'
         img.save(file_name)
 
-    @staticmethod
+    @ staticmethod
     def clean_string(string: str):
         # remove linebreaks from string
         string = string.replace('\n', ' ').replace('\r', ' ')
@@ -197,15 +304,11 @@ class Helper:
         print('clean_string: ' + string + ' now clean ...')
         return string
 
-    @staticmethod
-    def fix_spelling(string: str):
-        tool = language_tool_python.LanguageToolPublicAPI('de-DE')
-        string = tool.correct(string)
-        return string
-
 
 if __name__ == '__main__':
     dotenv.load_dotenv()
-    instabot = InstaBot(os.getenv('INSTA_USERNAME'), os.getenv('INSTA_PASSWORD'), True)
+    instabot = InstaBot(os.getenv('INSTA_USERNAME'),
+                        os.getenv('INSTA_PASSWORD'), True)
     instabot.login()
-    instabot.automated_mode()
+    instabot.create_post('Forest')
+    # instabot.automated_mode()
